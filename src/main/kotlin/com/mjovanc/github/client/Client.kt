03b7c23a59801b1072a865a4c0a0ac4b10e76ad7
mjovanc <mjovanc@icloud.com>
2023-04-12 11:51:52 +0200

    Adding Client and CollaboratorsClient

---
+++ src/main/kotlin/com/mjovanc/github/client/Client.kt
@@ -0,0 +1,12 @@
+package com.mjovanc.github.client
+
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+val client = HttpClient {
+    install(ContentNegotiation) {
+        json(Json { ignoreUnknownKeys = true })
+    }
+}
---
+++ src/main/kotlin/com/mjovanc/github/client/collaborators/CollaboratorsClient.kt
@@ -0,0 +1,19 @@
+package com.mjovanc.github.client.collaborators
+
+import org.slf4j.LoggerFactory
+
+/**
+ * Collaborators Client
+ *
+ * Implements the GitHub Collaborators API endpoints seen in
+ * https://docs.github.com/en/rest/collaborators
+ *
+ * @author Marcus Cvjeticanin
+ * @since 0.1.0
+ */
+class CollaboratorsClient {
+
+    private val logger = LoggerFactory.getLogger("CollaboratorsClient")
+
+
+}
