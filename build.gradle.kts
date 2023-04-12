a278be38ef602b024addf82b030d69aab6e4ad1b
mjovanc <mjovanc@icloud.com>
2023-04-12 11:22:46 +0200

    Updating build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,17 @@
+val ossrhUsername: String? = System.getProperty("ossrhUsername")
+val ossrhPassword: String? = System.getProperty("ossrhPassword") // this file should be in the HOME directory gradle.properties
+
 plugins {
     kotlin("jvm") version "1.8.10"
     id("org.jetbrains.dokka") version "1.8.10"
+    id("org.jetbrains.kotlinx.kover") version "0.7.0-Alpha"
+    `java-library`
+    `maven-publish`
+    signing
     application
 }
 
-group = "com.mjovanc.github-api"
+group = "com.mjovanc.github"
 version = "0.1.0-SNAPSHOT"
 
 repositories {
@@ -23,8 +30,97 @@ kotlin {
     jvmToolchain(17)
 }
 
-application {
-    mainClass.set("MainKt")
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+koverReport {
+    verify {
+        onCheck = true
+        rule {
+            isEnabled = true
+            entity = kotlinx.kover.gradle.plugin.dsl.GroupingEntityType.APPLICATION
+
+            bound {
+                minValue = 60
+                maxValue = 90
+                metric = kotlinx.kover.gradle.plugin.dsl.MetricType.LINE
+                aggregation = kotlinx.kover.gradle.plugin.dsl.AggregationType.COVERED_PERCENTAGE
+            }
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "github-api"
+            groupId = "com.mjovanc.github"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("Kotlin API for GitHub")
+                description.set("Kotlin API for GitHub")
+                url.set("https://github.com/mjovanc/github-api")
+                licenses {
+                    license {
+                        name.set("The 3-Clause BSD License")
+                        url.set("https://github.com/mjovanc/github-api/blob/master/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("mjovanc")
+                        name.set("Marcus Cvjeticanin")
+                        email.set("mjovanc@icloud.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:mjovanc/github-api.git")
+                    developerConnection.set("scm:git@github.com:mjovanc/github-api.git")
+                    url.set("https://github.com/mjovanc/github-api")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "Sonatype"
+            val host = "https://s01.oss.sonatype.org"
+            val path = if (version.toString().endsWith("SNAPSHOT")) "/content/repositories/snapshots/"
+            else "/service/local/staging/deploy/maven2/"
+            url = uri(host.plus(path))
+            println("> publish.url: $url")
+            println("> publish.path: $path")
+            println("> publish.version: $version")
+
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                username = ossrhUsername
+                password = ossrhPassword
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
 }
 
 tasks.register<Copy>("copyDokkaHtmlMultiModule") {
--- src/main/kotlin/com/mjovanc/github/Main.kt
+++ src/main/kotlin/com/mjovanc/github/Main.kt
@@ -1,3 +1,5 @@
+package com.mjovanc.github
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
